710ac422890fa02b87ca334f88b25086f5b7465c
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-02-04 15:54:55 +0530

    chess-app-landing-page-structure

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -2,13 +2,17 @@
 export const LandingPage = () => {
     return (
         <>
-            <div className="land-container flex">
-                <div>Welcome to 3D Chess Game</div>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <img className="w-50" src={"/chessboard.jpeg"} alt="3D Chess Board"/>
+            <div className="land-container flex justify-content-evenly">
+                <div className="desc-container text-center" style={{margin:"10rem auto 4rem"}}>
+                    <h1 className="">Welcome to 3D Chess Game</h1>
+                    <h2 className="mb-10">This is a multiplayer online chess game with a 3D environment.</h2>
+                    <button className="bg-blue-950">Play with AI</button>
+                    <button className="bg-blue-950">Play with a Friend</button>
+                </div>
+                <div className="img-container">
+                    <img className="w-full h-dvh" src={"/chessboard.jpeg"} alt="3D Chess Board"/>
                 </div>
             </div>
-            <button className="bg-red-200 ">Join here</button>
         </>
     );
 };
