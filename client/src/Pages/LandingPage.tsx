dbefdf7eec94689988d7631f558fba6d7a0d9aec
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-02-05 20:12:36 +0530

    chess-app-frontend-tailwindcss-landing-page-finishing

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -2,12 +2,14 @@
 export const LandingPage = () => {
     return (
         <>
-            <div className="land-container flex justify-content-evenly">
-                <div className="desc-container text-center" style={{margin:"10rem auto 4rem"}}>
-                    <h1 className="">Welcome to 3D Chess Game</h1>
-                    <h2 className="mb-10">This is a multiplayer online chess game with a 3D environment.</h2>
-                    <button className="bg-blue-950">Play with AI</button>
-                    <button className="bg-blue-950">Play with a Friend</button>
+            <div className="land-container flex justify-content-evenly" style={{backgroundColor:"#536d99"}}>
+                <div className="desc-container text-center" style={{margin:"10rem auto 5rem", color:"aliceblue"}}>
+                    <h1 className="mb-5">Welcome to 3D Chess Game</h1>
+                    <h2 className="mb-5">This is a multiplayer online chess game with a 3D environment.</h2>
+                    <div className="button-container" style={{marginLeft:"5rem", width:"44%"}}>
+                        <button className="bg-blue-950">Play with AI</button>
+                        <button className="bg-blue-950">Play with a Friend</button>
+                    </div>
                 </div>
                 <div className="img-container">
                     <img className="w-full h-dvh" src={"/chessboard.jpeg"} alt="3D Chess Board"/>
