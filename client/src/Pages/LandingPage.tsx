8ac1c177efaea49b2b35b3f2d4f5a956e260916e
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-02-06 18:45:01 +0530

    chess-app-frontend-tailwindcss-landing-page-play-buttons-setup

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -6,9 +6,9 @@ export const LandingPage = () => {
                 <div className="desc-container text-center" style={{margin:"10rem auto 5rem", color:"aliceblue"}}>
                     <h1 className="mb-5">Welcome to 3D Chess Game</h1>
                     <h2 className="mb-5">This is a multiplayer online chess game with a 3D environment.</h2>
-                    <div className="button-container" style={{marginLeft:"5rem", width:"44%"}}>
-                        <button className="bg-blue-950">Play with AI</button>
-                        <button className="bg-blue-950">Play with a Friend</button>
+                    <div className="button-container flex gap-5" style={{marginLeft:"18rem", width:"44%"}}>
+                        <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}}>Play with AI</button>
+                        <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}}>Play with a Friend</button>
                     </div>
                 </div>
                 <div className="img-container">
