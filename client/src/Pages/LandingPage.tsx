c3bfefd0311869e2f886c6218c37a4b0e6d1da9c
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-02-10 12:11:45 +0530

    chess-app-frontend-new-game-page setup

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -14,7 +14,7 @@ export const LandingPage = () => {
                     <h2 className="mb-5">This is a multiplayer online chess game with a 3D environment.</h2>
                     <div className="button-container flex gap-5" style={{marginLeft:"18rem", width:"44%"}}>
                         <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}}>Play with AI</button>
-                        <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}} onClick={()=>handleLetsPlay()}>Play with a Friend</button>
+                        <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}} onClick={()=>handleLetsPlay()}>Play with Friend</button>
                     </div>
                 </div>
                 <div className="img-container">
