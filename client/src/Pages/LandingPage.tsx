0219ad1dcd844d7c98be2f8014281a013efa28bb
Rinzlertron456 <18r01a04b3@cmritonline.ac.in>
2025-02-11 15:50:40 +0530

    chess-app-game-board-chess-board

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -12,7 +12,7 @@ export const LandingPage = () => {
                 <div className="desc-container text-center" style={{margin:"10rem auto 5rem", color:"aliceblue"}}>
                     <h1 className="mb-5">Welcome to 3D Chess Game</h1>
                     <h2 className="mb-5">This is a multiplayer online chess game with a 3D environment.</h2>
-                    <div className="button-container flex gap-5" style={{marginLeft:"18rem", width:"44%"}}>
+                    <div className="button-container flex gap-5" style={{marginLeft:"15rem", width:"44%"}}>
                         <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}}>Play with AI</button>
                         <button className=" btn bg-blue-950" style={{border:"2px solid aliceblue", color:"aliceblue"}} onClick={()=>handleLetsPlay()}>Play with Friend</button>
                     </div>
